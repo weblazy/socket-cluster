fee7adaa4273b8b453dd0f1b76c84c6a28f08176
liuguoqiang <liuguoqiang@xiaoheiban.cn>
2021-03-01 18:56:26 +0800

    feat: buffer

--- protocol/quic_protocol/quic_protocol.go
+++ protocol/quic_protocol/quic_protocol.go
@@ -8,12 +8,19 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
-	"io"
 	"math/big"
+	"sync"
 
 	"github.com/lucas-clemente/quic-go"
 	"github.com/weblazy/socket-cluster/protocol"
 )
+
+const HEAD_SIZE = 4
+const HEADER = "BEGIN"
+
+// 每个消息(包括头部)的最大长度， 这里最大可以设置4G
+const BUFFER_LENGTH = 1024 * 70
+const CHAN_MSG_COUNT = 2
 
 type QuicProtocol struct {
 	ConnectHandler protocol.Node
@@ -52,20 +59,33 @@ func (this *QuicProtocol) ListenAndServe(port int64) error {
 }
 
 func handleClient(sess quic.Session) {
+	msg := make(chan string, 10) // 这里设置消息channel可以容纳10个消息
+	// 缓存区设置最大为4G字节， 如果单个消息大于这个值就不能接受了
 	stream, err := sess.AcceptStream(context.Background())
 	if err != nil {
 		panic(err)
-	} else {
-		for {
-			buf := make([]byte, 1024)
-			_, err = io.ReadFull(stream, buf)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Printf("Client: Got '%s'\n", buf)
-		}
 	}
+	buffer1 := protocol.NewBuffer(stream, HEADER, BUFFER_LENGTH)
+
+	var wg sync.WaitGroup
+	wg.Add(2) // 主的routine将等待两个routine(读消息, 打印消息)的完成
+	go func() {
+		doMsg(msg)
+		defer wg.Add(-1)
+	}()
+	go func() {
+		err := buffer1.Read(msg)
+		if err != nil {
+			if err.Error() == "EOF" {
+				close(msg) // 对等方关闭了, 这里关闭chan, 通知接收消息的routine别等了，人家都关了
+			} else {
+				panic(err)
+			}
+		}
+		defer wg.Add(-1)
+	}()
+	wg.Wait()
+	fmt.Println("一个客户端处理的消息处理完毕")
 }
 
 // Setup a bare-bones TLS config for the server
@@ -88,4 +108,12 @@ func generateTLSConfig() *tls.Config {
 		panic(err)
 	}
 	return &tls.Config{NextProtos: []string{"quic-echo-example"}, Certificates: []tls.Certificate{tlsCert}}
+}
+
+func doMsg(msg chan string) {
+	count := 0
+	for v := range msg {
+		fmt.Println("第", count, "个消息体长:", len(v))
+		count++
+	}
 }
